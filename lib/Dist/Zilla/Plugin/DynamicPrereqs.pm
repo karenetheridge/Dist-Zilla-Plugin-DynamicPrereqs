7b0aba5b6b013a88a395e874ea004d12104d938a
Karen Etheridge <ether@cpan.org>
2014-05-10 11:35:19 -0700

    initial code, tests, docs

--- lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
+++ lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
@@ -1,31 +1,143 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::DynamicPrereqs;
-# ABSTRACT: ...
+# ABSTRACT: Specify dynamic (user-side) prerequisites for your distribution
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose;
+with
+    'Dist::Zilla::Role::InstallTool',
+    'Dist::Zilla::Role::MetaProvider',
+    'Dist::Zilla::Role::AfterBuild',
+;
+use List::Util 'first';
+use namespace::autoclean;
 
+has raw => (
+    isa => 'ArrayRef[Str]',
+    traits => ['Array'],
+    handles => { raw => 'elements' },
+    lazy => 1,
+    default => sub { [] },
+);
 
-1;
+sub mvp_multivalue_args { qw(raw) }
+
+sub mvp_aliases { +{ '-raw' => 'raw' } }
+
+sub metadata { return +{ dynamic_config => 1 } }
+
+sub after_build
+{
+    my $self = shift;
+    $self->log_fatal('Build.PL detected - dynamic prereqs will not be added to it!')
+        if first { $_->name eq 'Build.PL' } @{ $self->zilla->files };
+}
+
+sub setup_installer
+{
+    my $self = shift;
+
+    my $file = first { $_->name eq 'Makefile.PL' } @{$self->zilla->files};
+    my $content = $file->content;
+
+    $self->log_debug('Inserting dynamic prereq into Makefile.PL...');
+
+    # insert after declarations for BOTH %WriteMakefileArgs, %FallbackPrereqs.
+    # TODO: if marker cannot be found, fall back to looking for just
+    # %WriteMakefileArgs -- this requires modifying the content too.
+    $self->log_fatal('failed to find position in Makefile.PL to munge!')
+        if $content !~ m'^my %FallbackPrereqs = \(\n(?:[^;]+)^\);$'mg;
+
+    my $pos = pos($content);
+
+    $content = substr($content, 0, $pos)
+        . "\n\n"
+        . join("\n", $self->raw)
+        . "\n" . substr($content, $pos, -1);
+
+    $file->content($content);
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =pod
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Plugin::DynamicPrereqs;
+In your F<dist.ini>:
 
-    ...
+    [DynamicPrereqs]
+    -raw = $WriteMakefileArgs{PREREQ_PM}{'Role::Tiny'} = $FallbackPrereqs{'Role::Tiny'} = '1.003000'
+    -raw = if eval { require Role::Tiny; 1 };
 
 =head1 DESCRIPTION
 
-...
+This is a L<Dist::Zilla> plugin that inserts code into your F<Makefile.PL> to
+indicate dynamic (installer-side) prerequisites.
 
-=head1 FUNCTIONS/METHODS
+Code is inserted immediately after the declarations for C<%WriteMakefileArgs>
+and C<%FallbackPrereqs>, before they are conditionally modified when an older
+L<ExtUtils::MakeMaker> is installed.  This gives you an opportunity to add to
+the C<WriteMakefile> arguments: C<PREREQ_PM>, C<BUILD_REQUIRES>, and
+C<TEST_REQUIRES>, and therefore modify the prerequisites in the user's
+F<MYMETA.yml> and F<MYMETA.json> based on conditions found on the user's system.
 
-=head2 C<foo>
+The C<dynamic_config> field in L<metadata|CPAN::Meta::Spec/dynamic_config> is
+already set for you.
 
-...
+=for stopwords usecase
+
+You could potentially use this plugin for performing other modifications in
+F<Makefile.PL> other than user-side prerequisite modifications, but I can't
+think of a situation where this makes sense. Contact me if you have any ideas!
+
+Only F<Makefile.PL> modification is supported at this time. This author
+considers the use of L<Module::Build> to be questionable in all circumstances,
+and L<Module::Build::Tiny> does not (yet?) support dynamic configuration.
+
+=head1 CONFIGURATION OPTIONS
+
+=head2 C<-raw>
+
+The code to be inserted; must be valid and complete perl statements. You can
+reference and modify the already-declared C<%WriteMakefileArgs> and
+C<%FallbackPrereqs> variables, as inserted into F<Makefile.PL> by
+L<Dist::Zilla::Plugin::MakeMaker> and subclasses (e.g.
+L<Dist::Zilla::Plugin::MakeMaker::Awesome> since L<Dist::Zilla> C<5.001>.
+
+This option can be used more than once; lines are added in the order in which they are provided.
+
+If you use external libraries in the code you are inserting, you B<must> add
+these modules to C<configure_requires> prereqs in metadata (e.g. via
+C<[Prereqs / ConfigureRequires]> in your F<dist.ini>).
+
+=for Pod::Coverage mvp_multivalue_args mvp_aliases metadata after_build setup_installer
+
+=head1 WARNING: UNSTABLE API!
+
+=for stopwords DarkPAN
+
+This plugin is still undergoing active development, and the interfaces B<will>
+change and grow as I work through the proper way to do various things.  As I
+make changes, I will be using L<http://grep.cpan.me> to find and fix any
+upstream users, but I obviously cannot do this for DarkPAN users. Regardless,
+please contact me (see below) and I will keep you directly advised of
+interface changes.
+
+Future options may include:
+
+=begin :list
+
+* C<-condition> a Perl expression that is tested before additional prereqs are added
+* C<-phase> the phase in which subsequently-specified module/version pairs will be added
+* C<-runtime> a module and version that is added to runtime prereqs should the C<-condition> be satisfied
+* C<-test> a module and version that is added to test prereqs should the C<-condition> be satisfied
+* C<-build> a module and version that is added to build prereqs should the C<-condition> be satisfied
+
+=end :list
 
 =head1 SUPPORT
 
@@ -35,15 +147,12 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Plugin-DynamicPrereqs@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-DynamicPrereqs@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Dist::Zilla::Plugin::MakeMaker>
+* L<ExtUtils::MakeMaker/Using Attributes and Parameters>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,115 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
-use Dist::Zilla::Plugin::DynamicPrereqs;
+use Test::DZil;
+use Test::Fatal;
+use Path::Tiny;
+use File::pushd 'pushd';
+use Test::Deep;
+use Test::Deep::JSON;
 
-fail('this test is TODO!');
+my $tzil = Builder->from_config(
+    { dist_root => 't/does-not-exist' },
+    {
+        add_files => {
+            path(qw(source dist.ini)) => simple_ini(
+                [ GatherDir => ],
+                [ MetaJSON => ],
+                [ Prereqs => { 'strict' => '0', 'Test::More' => '0' } ],
+                [ MakeMaker => ],
+                [ DynamicPrereqs => {
+                        raw => [
+                            q|$WriteMakefileArgs{PREREQ_PM}{'Test::More'} = $FallbackPrereqs{'Test::More'} = '0.123'|,
+                            q|if eval { require Test::More; 1 };|,
+                        ],
+                    },
+                ],
+            ),
+            path(qw(source lib Foo.pm)) => "package Foo;\n1;\n",
+        },
+    },
+);
+
+$tzil->chrome->logger->set_debug(1);
+is(
+    exception { $tzil->build },
+    undef,
+    'build proceeds normally',
+) or diag "log messages:\n", join("\n", @{ $tzil->log_messages });
+
+my $build_dir = path($tzil->tempdir)->child('build');
+
+my $meta_json = $build_dir->child('META.json')->slurp_raw;
+cmp_deeply(
+    $meta_json,
+    json(superhashof({
+        dynamic_config => 1,
+        prereqs => {
+            configure => {
+                requires => {
+                    'ExtUtils::MakeMaker' => ignore,
+                },
+            },
+            runtime => {
+                requires => {
+                    'strict' => '0',
+                    'Test::More' => '0',
+                },
+            },
+        },
+    })),
+    'dynamic_config set to 1 in metadata; static prereqs are in place',
+)
+or diag "found META.json content:\n", $meta_json;
+
+
+my $file = $build_dir->child('Makefile.PL');
+ok(-e $file, 'Makefile.PL created');
+
+my $makefile = $file->slurp_utf8;
+unlike($makefile, qr/[^\S\n]\n/m, 'no trailing whitespace in modified file');
+
+isnt(
+    index(
+        $makefile,
+        "\n\n"
+        . q|$WriteMakefileArgs{PREREQ_PM}{'Test::More'} = $FallbackPrereqs{'Test::More'} = '0.123'|
+        . "\n"
+        . q|if eval { require Test::More; 1 };|
+        . "\n\n"
+    )
+    -1,
+    'code inserted into Makefile.PL',
+) or diag "found Makefile.PL content:\n", $makefile;
+
+{
+    my $wd = pushd $build_dir;
+    $tzil->plugin_named('MakeMaker')->build;
+}
+
+my $mymeta_json = $build_dir->child('MYMETA.json')->slurp_raw;
+cmp_deeply(
+    $mymeta_json,
+    json(superhashof({
+        dynamic_config => 0,
+        prereqs => {
+            configure => {
+                requires => {
+                    'ExtUtils::MakeMaker' => ignore,
+                },
+            },
+            runtime => {
+                requires => {
+                    'strict' => '0',
+                    'Test::More' => '0.123',
+                },
+            },
+            build => ignore,    # always added by EUMM?
+            test => ignore,     # always added by EUMM?
+        },
+    })),
+    'dynamic_config reset to 0 in MYMETA; dynamic prereqs have been added',
+)
+or diag "found MYMETA.json content:\n", $mymeta_json;
 
 done_testing;
