2491310eabe9bfa48aa3bf43423967ef9ced0e45
Karen Etheridge <ether@cpan.org>
2014-06-14 21:24:14 -0700

    add some keywords to distribution metadata

--- lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
+++ lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::DynamicPrereqs;
 # ABSTRACT: Specify dynamic (user-side) prerequisites for your distribution
+# KEYWORDS: plugin distribution metadata MYMETA prerequisites Makefile.PL dynamic
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
