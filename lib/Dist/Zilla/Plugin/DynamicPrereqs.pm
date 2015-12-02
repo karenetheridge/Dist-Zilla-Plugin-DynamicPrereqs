15191474615250df4a30b95a2bbf76e7c58a113e
Karen Etheridge <ether@cpan.org>
2015-12-01 19:41:32 -0800

    increment $VERSION after 0.018 release

--- lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
+++ lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::DynamicPrereqs;
 # KEYWORDS: plugin distribution metadata MYMETA prerequisites Makefile.PL dynamic
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 use Moose;
 with
