91cd24245f0e9579820c5a4774cb0b56aa1257b3
Karen Etheridge <ether@cpan.org>
2017-12-20 16:50:01 -0800

    increment $VERSION after 0.034 release

--- lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
+++ lib/Dist/Zilla/Plugin/DynamicPrereqs.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::DynamicPrereqs;
 # ABSTRACT: Specify dynamic (user-side) prerequisites for your distribution
 # KEYWORDS: plugin distribution metadata MYMETA prerequisites Makefile.PL dynamic
 
-our $VERSION = '0.034';
+our $VERSION = '0.035';
 
 use Moose;
 with
